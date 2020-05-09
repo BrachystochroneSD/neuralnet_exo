b2b25bc7bc251306349d3958948907ee26fc3ba5
Samuel D <samrenfou@hotmail.com>
2020-05-09 15:57:36 +0200

    add maier and update

--- neuralnet.py
+++ neuralnet.py
@@ -2,7 +2,7 @@ from Modules.Neural_Network.Neural_Network import NeuralNet
 from Modules.Matrix.Matrix import Matrix
 import random
 
-nn=NeuralNet(2,2,1)
+nn=NeuralNet(2,10,10,1)
 
 ia = [([1,1], [0]),
       ([0,1], [1]),
@@ -10,11 +10,12 @@ ia = [([1,1], [0]),
       ([0,0], [0])]
 
 # Just a xor test
-for i in range(20000):
+for i in range(10000):
     index = random.randint(0,len(ia)-1)
-    i,a = ia[index]
-    nn.train(i,a)
-    nn.adjust(1)
+    inp,ans = ia[index]
+    nn.train(inp,ans)
+    if (i % 10) == 0:
+        nn.adjust(1)
 
 print(nn.output([1,1]))
 print(nn.output([0,0]))
